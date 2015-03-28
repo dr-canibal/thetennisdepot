9608f74b89fde4d664285e50da0af3c44e552e5f
Evgeniy Kononov <ekononov@mementia.com>
2015-03-28 19:51:46 +0200

    Tmp

---
+++ public/app/etc/tmp.php
@@ -0,0 +1,10 @@
+<?php
+
+$_host              = 'localhost';
+$_username          = 'root';
+$_password          = 'QWEqwe123';
+$_name              = 'tennis3';
+$_prefix            = 'dxov_';
+$_initStatements    = 'SET NAMES utf8';
+$_model             = 'mysql4';
+$_type              = 'pdo_mysql';
